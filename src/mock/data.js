08ebae213663d852e3cd00561a5a34fc9a6bd984
Thomas Rughooputh <thomas.rughooputh@porsche.digital>
2024-10-24 19:52:50 +0200

    Add mock data

---
+++ src/mock/data.js
@@ -0,0 +1,28 @@
+const cars = [
+  {
+    name: "Porsche 718 Cayman",
+    url: "https://files.porsche.com/filestore/image/multimedia/none/982-718-c7s-modelimage-sideshot/model/af021b27-d5ba-11ec-80ef-005056bbdc38/porsche-model.png",
+  },
+  {
+    name: "Porsche 911 Targa 4 GTS",
+    url: "https://files.porsche.com/filestore/image/multimedia/none/992-ta4-gts-modelimage-sideshot/model/fc3d2747-d31e-11eb-80d9-005056bbdc38/porsche-model.png",
+  },
+  {
+    name: "Porsche 911 GT3 RS",
+    url: "https://files.porsche.com/filestore/image/multimedia/none/992-gt3-rs-modelimage-sideshot/model/cfbb8ed3-1a15-11ed-80f5-005056bbdc38/porsche-model.png",
+  },
+  {
+    name: "Porsche Panamera Platinum Edition",
+    url: "https://files.porsche.com/filestore/image/multimedia/none/971-g2-2nd-4-pe-modelimage-sideshot/model/a4d14bd5-413f-11ec-80e2-005056bbdc38/porsche-model.png",
+  },
+  {
+    name: "Porsche Taycan Turbo S",
+    url: "https://files.porsche.com/filestore/image/multimedia/none/j1-taycan-tu-s-modelimage-sideshot/model/60c967a4-ac79-11e9-80c4-005056bbdc38/porsche-model.png",
+  },
+  {
+    name: "Porsche Macan GTS",
+    url: "https://files.porsche.com/filestore/image/multimedia/none/pa3-gts-modelimage-sideshot/model/4cc948bd-de44-11eb-80d9-005056bbdc38/porsche-model.png",
+  },
+];
+
+export default cars;
