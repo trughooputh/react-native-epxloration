8a0668b7890de361973a4bc7925ad0815d4b8946
Thomas Rughooputh <thomas.rughooputh@porsche.digital>
2024-10-27 13:47:45 +0100

    TS support

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/components/main';
-import { colors } from './src/styles/themes';
-import { fontsSize, margin } from './src/styles/helpers';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+// Components
+import Header from '@components/_core/header';
+import Main from '@components/main';
+
+// Styles
+import { colors } from '@styles/themes';
+import { margin } from '@styles/spacing';
+import { fontsSize } from '@styles/fonts';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>RN Dictionary App</Text>
-      <Main />
-    </View>
+    <SafeAreaView>
+      <View style={styles.main}>
+        <Text style={styles.title}>React Native Masterclass App</Text>
+        <Main />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: colors.main.primary,
-    alignItems: "left"
+    backgroundColor: colors.main.primary
   },
   title: {
     fontSize: fontsSize.L,
