3d7881863b179d5a65d68ffb5dd05e802e6fd1dd
Thomas Rughooputh <thomas.rughooputh@porsche.digital>
2024-10-27 12:00:26 +0100

    Babel config for aliases, jsconfig adjustments

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@components": "./src/components",
+            "@utils": "./src/utils",
+            "@styles": "./src/styles",
+            "@assets": "./src/assets",
+            "@config": "./src/config",
+            "@api": "./src/api"
+          }
+        }
+      ]
+    ]
   };
 };
